5d0f7cf7b984d1db0c098cd18d77e85662bb09c5
Aleksey <aleksey_naumenko@ukr.net>
2020-02-26 14:52:55 +0200

    Updates

---
+++ precache-manifest.5ac831b43ceab8c4950a631e04281400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76cb78cdf10f4edc4c13e2edaf8213e",
+    "url": "/calendar-on-react/index.html"
+  },
+  {
+    "revision": "4e3e78c55ce304f8af64",
+    "url": "/calendar-on-react/static/css/main.a23fb32c.chunk.css"
+  },
+  {
+    "revision": "e5c6b4c0dd55e8476f9b",
+    "url": "/calendar-on-react/static/js/2.46ed3f03.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calendar-on-react/static/js/2.46ed3f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3e78c55ce304f8af64",
+    "url": "/calendar-on-react/static/js/main.d55415d8.chunk.js"
+  },
+  {
+    "revision": "7192312db84795873a26",
+    "url": "/calendar-on-react/static/js/runtime-main.5c3784e2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Aleksey-Naumenko/calendar-on-react/precache-manifest.2a4d211847d13185f3ee8b162eebb7c8.js"
+  "/calendar-on-react/precache-manifest.5ac831b43ceab8c4950a631e04281400.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Aleksey-Naumenko/calendar-on-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calendar-on-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
