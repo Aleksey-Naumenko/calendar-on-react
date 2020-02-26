26b9e4e0c82beee8af8d57f64512e128711d2567
Aleksey <aleksey_naumenko@ukr.net>
2020-02-26 14:25:07 +0200

    Updates

---
+++ precache-manifest.2a4d211847d13185f3ee8b162eebb7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "864837f64a3c33ab88767736d6d5f06b",
+    "url": "/Aleksey-Naumenko/calendar-on-react/index.html"
+  },
+  {
+    "revision": "4e3e78c55ce304f8af64",
+    "url": "/Aleksey-Naumenko/calendar-on-react/static/css/main.a23fb32c.chunk.css"
+  },
+  {
+    "revision": "e5c6b4c0dd55e8476f9b",
+    "url": "/Aleksey-Naumenko/calendar-on-react/static/js/2.46ed3f03.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Aleksey-Naumenko/calendar-on-react/static/js/2.46ed3f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3e78c55ce304f8af64",
+    "url": "/Aleksey-Naumenko/calendar-on-react/static/js/main.d55415d8.chunk.js"
+  },
+  {
+    "revision": "f0de7cdb64789c4e4077",
+    "url": "/Aleksey-Naumenko/calendar-on-react/static/js/runtime-main.b3bf1e89.js"
+  }
+]);
